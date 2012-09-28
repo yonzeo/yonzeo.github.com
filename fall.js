00f0268e157c2e0e82da316deeb0d706854bbc95
yonzeo <yonzeo@qq.com>
2012-09-28 09:52:40 +0800

    waterfall

--- fall.js
+++ fall.js
@@ -3,7 +3,6 @@ var li = $('li');
 function liuxiaofan(){
     var h = [];
     var li_W = $(li[0]).outerWidth() + margin*3;
-    document.write($(li[0]).outerWidth(true));document.write(' ' + $(li[0]).outerHeight(true));
     var n = document.documentElement.offsetWidth / li_W | 0;
     for(var i = 0; i < li.length; i++ ){
         li_H = $(li[i]).outerHeight();
